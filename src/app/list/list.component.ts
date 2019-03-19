9ff4fc329eb68f95006b54f42e1636156168a304
Elie <elie.bironneau@capgemini.com>
2019-03-19 11:43:02 +0100

    Sender  + Listing

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { DatePipe } from '@angular/common';
-import { environment } from '@env/environment';
 import { ListService } from './list.service';
 import { Phrase } from '../shared/dto/phrase';
 import { finalize } from 'rxjs/operators';
--- src/app/sender/sender.component.ts
+++ src/app/sender/sender.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { DatePipe } from '@angular/common';
-import { environment } from '@env/environment';
 import { SenderService } from './sender.service';
 import { Phrase } from '../shared/dto/phrase';
 import { finalize } from 'rxjs/operators';
